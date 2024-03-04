1620bac74875aa64492b7d4e63f5e8c2b7dbc501
EC2 Default User <ec2-user@ip-172-31-57-240.ec2.internal>
2024-03-04 20:18:23 +0000

    Updating module

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/hrmcardle0/go-multi-version-module
 
 go 1.20
+
+require github.com/golang/protobuf v1.5.3 // indirect
---
+++ module.go
@@ -0,0 +1,10 @@
+package module
+
+import (
+	"fmt"
+)
+
+func ModuleFunc() string {
+	fmt.Println("Module called!")
+	return "Module returning"
+}
